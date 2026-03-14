ea31a12c752a38dba41a1c829f220edd49287b0d
pseudoSKB <skbarefin05@gmail.com>
2026-03-14 22:42:01 +0600

    Added kaizen logo and tagline

--- front_end/src/landingPg.jsx
+++ front_end/src/landingPg.jsx
@@ -1,7 +1,15 @@
 import "./landingPg.css";
+import kaizenLogo from "./assets/kaizen-logo.png";
 
 function LandingPg() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="logo-section">
+        <img src={kaizenLogo} alt="Kaizen Logo" className="logo" />
+        <p className="tagline">Your health, Simplified</p>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPg;

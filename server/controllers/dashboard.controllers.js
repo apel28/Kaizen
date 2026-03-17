c69e34669101c755f9c02c3cff9fef32e7f82078
pseudoSKB <skbarefin05@gmail.com>
2026-03-17 16:14:05 +0600

    New server of dashboard logic

--- server/controllers/dashboard.controllers.js
+++ server/controllers/dashboard.controllers.js
@@ -1,47 +1,55 @@
-import * as patientCode from "../query/patient.js";
+import * as patientCode from "../query/patient.js"
 
 export async function patientDashboardData(req, res) {
-  const user_id = req.user.user_id; //attached from the auth middleware using token
-  const patient_id = await patientCode.getPatientId(user_id);
-  if (patient_id == null) {
-    return res.json({
-      bp: {
-        systolic: null,
-        diastolic: null,
-      },
-      "heart rate": null,
-      "blood sugar": null,
-      height: null,
-      weight: null,
-    });
-  }
-  const vitals = await patientCode.getLatestVitals(patient_id);
-  if (vitals == null) {
-    return res.json({
-      bp: {
-        systolic: null,
-        diastolic: null,
-      },
-      "heart rate": null,
-      "blood sugar": null,
-      height: null,
-      weight: null,
-    });
-  }
+    const user_id = req.user.user_id; //attached fromt the auth middleware using token
+    const patient_prof = await patientCode.getPatientProfile(user_id);
+    if(patient_prof.patient_id == null) {
+        return res.json({
+            'bp' : {
+                'systolic' : null,
+                'diastolic': null,
+            },
+            'heart rate': null,
+            'blood sugar': null,
+            'bmi': null,
+            'name': null,
+            'patientId': null,
+        });
+    }
+    const vitals = await patientCode.getLatestVitals(patient_prof.patient_id);
+    if(vitals == null) {
+        return res.json({
+            'bp' : {
+                'systolic' : null,
+                'diastolic': null,
+            },
+            'heart rate': null,
+            'blood sugar': null,
+            'bmi': null,
+            'name': null,
+            'patientId': null,
+        });
+    }
+ 
+    const bp = vitals.bp.split('/');
 
-  const bp = vitals.bp.split("/");
+    const bmi = vitals.weight/(vitals.height*vitals.height);
 
-  return res.json({
-    bp: {
-      systolic: Number(bp[0]),
-      diastolic: Number(bp[1]),
-    },
-    "heart rate": vitals.heart_rate,
-    "blood sugar": vitals.blood_sugar,
-    height: vitals.height,
-    weight: vitals.weight,
-  });
-}
+    const patient_name = patient_prof.first_name + " " + (patient_prof.middle_name ? patient_prof.middle_name + " " : "") + patient_prof.last_name
+
+    return res.json({
+        'bp' : {
+            'systolic': Number(bp[0]),
+            'diastolic': Number(bp[1]),
+        },
+        'heart rate': vitals.heart_rate,
+        'blood sugar': vitals.blood_sugar,
+        'bmi': bmi,
+        'name': patient_name,
+        'patientId': patient_prof.patient_id,
+    });
+
+  }
 
 // async function test() {
 //     const req = {
--- server/controllers/register.controllers.js
+++ server/controllers/register.controllers.js
@@ -1,168 +1,128 @@
 import pool from "../db.js";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
 
-function generatorId() {
-  const chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  return Array.from(
-    { length: 9 },
-    () => chars[Math.floor(Math.random() * chars.length)],
-  ).join("");
-}
+
+function generatorId(){
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  return Array.from({ length: 9 }, () => chars[Math.floor(Math.random() * chars.length)]).join("");
+};
+
 
 async function checkUserId(userID) {
-  const result = await pool.query(
-    `
+    const result = await pool.query(`
         SELECT * FROM "user" WHERE user_id = $1;
-        `,
-    [userID],
-  );
-  return result.rows.length == 0;
+        `, [userID]);
+    return result.rows.length == 0;
 }
 
 async function genUserId(role) {
-  while (true) {
-    const userID = role + generatorId();
-    const isAvailable = await checkUserId(userID);
-    if (!isAvailable) continue;
-    return userID;
-  }
+    while(true) {
+        const userID = role + generatorId();
+        const isAvailable = await checkUserId(userID);
+        if(!isAvailable) continue;
+        return userID; 
+    }
 }
 
 async function checkMailExists(email) {
-  const result = await pool.query(
-    `
+    const result = await pool.query(`
         SELECT * FROM "user" WHERE email = $1;
-        `,
-    [email],
-  );
-  return result.rows.length != 0;
+        `, [email]);
+    return result.rows.length != 0;
 }
 
 async function personExists(nid) {
-  const result = await pool.query(
-    `
+    const result = await pool.query(`
         SELECT * FROM profile 
         WHERE nid = $1;
-        `,
-    [nid],
-  );
-  return result.rows.length != 0;
+        `, [nid]);
+    return result.rows.length != 0;
 }
 
 export async function register(req, res) {
-  const {
-    first_name,
-    middle_name, // null
-    last_name,
-    date_of_birth,
-    address, //null
-    contact_info,
-    emergency_contact, //null
-    gender, //null
-    nid,
-    nationality, //null
-    email,
-    password,
-    role,
-  } = req.body;
+    const {
+        first_name,
+        middle_name, // null
+        last_name,
+        date_of_birth,
+        address, //null
+        contact_info, 
+        emergency_contact, //null
+        gender, //null
+        nid, 
+        nationality, //null
+        email, 
+        password,
+        role
+    } = req.body;
 
-  if (await checkMailExists(email)) {
-    return res.status(409).json({
-      error: "Mail exists",
-    });
-  }
+    
+    if(await checkMailExists(email)) {
+        return res.status(409).json({
+            error:"Mail exists",
+        });
+    }
+    
+    if(await personExists(nid)) {
+        return res.status(409).json({
+            error:"Person exisits",
+        });
+    }
 
-  if (await personExists(nid)) {
-    return res.status(409).json({
-      error: "Person exisits",
-    });
-  }
+    const user_id = await genUserId(role);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-  const user_id = await genUserId(role);
-  const hashedPassword = await bcrypt.hash(password, 12);
+    console.log(user_id);
 
-  console.log(user_id);
-
-  const queryResultUser = await pool.query(
-    `
+    const queryResultUser = await pool.query(`
         INSERT INTO "user"
         VALUES ($1, $2, $3)
         RETURNING *; 
-        `,
-    [user_id, hashedPassword, email],
-  );
+        `,[user_id, hashedPassword, email]);
 
-  const queryResultProfile = await pool.query(
-    `
+    const queryResultProfile = await pool.query(`
         INSERT INTO profile
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
         RETURNING *;
-        `,
-    [
-      user_id,
-      first_name,
-      middle_name,
-      last_name,
-      date_of_birth,
-      address,
-      contact_info,
-      emergency_contact,
-      gender,
-      nid,
-      nationality,
-    ],
-  );
+        `, [user_id, first_name, middle_name, last_name, date_of_birth, address, contact_info, emergency_contact, gender, nid, nationality]);
 
-  const queryResultRole = await pool.query(
-    `
+    
+    const queryResultRole = await pool.query(`
         INSERT INTO "role"(user_id, role_id)
         VALUES ($1, $2);
-        `,
-    [user_id, role],
-  );
+        `, [user_id, role]);
 
-  if (role == 1) {
-    const queryResultRole = await pool.query(
-      `
+    if(role == 1) {
+        const queryResultRole = await pool.query(`
         INSERT INTO "doctor"(user_id)
         VALUES ($1);
-        `,
-      [user_id],
-    );
-  }
+        `, [user_id]);
+    }
 
-  if (role == 2) {
-    const queryResultRole = await pool.query(
-      `
+    if(role == 2) {
+        const queryResultRole = await pool.query(`
         INSERT INTO "patient"(user_id)
         VALUES ($1);
-        `,
-      [user_id],
-    );
-  }
+        `, [user_id]);
+    }
 
-  if (role == 3) {
-    const queryResultRole = await pool.query(
-      `
+    if(role == 3) {
+        const queryResultRole = await pool.query(`
         INSERT INTO "nurse"(user_id)
         VALUES ($1);
-        `,
-      [user_id],
-    );
-  }
+        `, [user_id]);
+    }
 
-  if (role == 4) {
-    const queryResultRole = await pool.query(
-      `
+    if(role == 4) {
+        const queryResultRole = await pool.query(`
         INSERT INTO "staff"(user_id)
         VALUES ($1);
-        `,
-      [user_id],
-    );
-  }
-
-  res.status(201).json({
-    result: "ok",
-  });
+        `, [user_id]);
+    }
+    
+    res.status(201).json({
+        result:"ok",
+    })
 }
+
+

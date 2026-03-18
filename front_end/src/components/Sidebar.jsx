c8f9778a122c82e22f0c65d9a55d8de0795d14f9
pseudoSKB <skbarefin05@gmail.com>
2026-03-19 04:06:20 +0600

    Added logout feature

--- front_end/src/components/Sidebar.jsx
+++ front_end/src/components/Sidebar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import kaizenLogo from "../assets/kaizen-logo.webp";
-import {House, Calendar, FileText, Clock, FlaskConical, Pill} from "lucide-react";
+import {House, Calendar, FileText, Clock, FlaskConical, Pill, LogOut} from "lucide-react";
+import { useAuth } from "../context/AuthContext";
 
 const Sidebar = ({ idx }) => {
+
+  const {logout} = useAuth();
+
   const navItems = [
     "Home",
     "Appointment",
     "Prescription",
     "Schedule",
     "Tests",
-    "Medication",
+    "Medication"
   ];
 
   const navIcons = [
@@ -19,6 +23,7 @@ const Sidebar = ({ idx }) => {
     <Clock key="schedule" />,
     <FlaskConical key="tests" />,
     <Pill key="medication" />,
+    <LogOut key="logout" />
   ];
 
   return (
@@ -44,6 +49,16 @@ const Sidebar = ({ idx }) => {
               </div>
             </li>
           ))}
+          <button
+            key="logout"
+            onClick={logout}
+            className={`mb-4 p-3 absolute bottom-4 rounded-lg cursor-pointer transition-all hover:bg-red-600/50`}
+          >
+            <div className="flex items-center gap-2">
+              <LogOut key="logout" />
+              Log Out
+            </div>
+          </button>
         </ul>
       </nav>
     </aside>

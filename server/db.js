ce8d94e9012863588928e5650a5e3b016c912be4
Apel <saurav15152@gmail.com>
2026-04-06 22:08:33 +0600

    fix again 2

--- server/db.js
+++ server/db.js
@@ -8,13 +8,17 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.join(__dirname, '.env') });
 
-const pool = new Pool ({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-});
+const config = process.env.DATABASE_URL 
+  ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+  : {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME
+    };
+
+const pool = new Pool(config);
 
 export default pool;
 
